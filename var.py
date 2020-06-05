d10a973fae76dca40776c48d728479d97df86ac5
Shah Imran <shahimrans64@gmail.com>
2020-06-05 20:14:38 +0600

    Added developed by

--- var.py
+++ var.py
@@ -78,5 +78,7 @@ try:
     about = ""
     with open('about.txt', 'r', encoding='utf-8') as f:
         about = f.read()
+
+    about += "Developed By - Shah Imran\nSkype_id - shah_imran_sust@outlook.com"
 except Exception as e:
     print("Exeception occured at config loading:{}".format(e))
